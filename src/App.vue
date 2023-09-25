57f3ba47789db9fde63066de0b3bb69647b54f88
tour_2st <fk.easel@gmail.com>
2023-09-25 21:04:58 +0900

    [update] change panel.vue conponents and devide method into app.vue to make it moving smooth

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,102 @@
 <script setup>
 import panel from './components/panel.vue'
+import { reactive, ref } from 'vue'
+
+var selectedindex = ref(0)
+var isselected = ref(false)
+
+const panel_data = reactive([
+  {
+    letter: "あ",
+    x: 0,
+    y: 100,
+    x_offset: 0,
+    y_offset: 0,
+    status: "hit",
+    selected: false
+  },
+  {
+    letter: "い",
+    x: 0,
+    y: 0,
+    x_offset: 0,
+    y_offset: 0,
+    status: "blow",
+    selected: false
+  },
+  {
+    letter: "う",
+    x: 0,
+    y: 200,
+    x_offset: 0,
+    y_offset: 0,
+    status: "miss",
+    selected: false
+  }
+])
+
+//パネル上でマウスを押したときの動作
+function mousedown(event, index)
+{
+  //selectedindex を 選択中にする
+  this.selectedindex = index
+  this.isselected = true
+
+  //パネルとカーソルの間の差分
+  this.panel_data[this.selectedindex].x_offset = event.x - this.panel_data[this.selectedindex].x
+  this.panel_data[this.selectedindex].y_offset = event.y - this.panel_data[this.selectedindex].y
+
+  this.panel_data[this.selectedindex].selected = true
+}
+//パネル上でマウスを離したときの動作
+function mouseup(event, index)
+{
+  //選択中を外す
+  this.isselected = false
+  this.panel_data[this.selectedindex].selected = false
+  //選択中のインデックスと座標を用いて操作を実行
+}
+//画面上でマウスを操作した際の動作
+function mousemove(event)
+{
+  //選択中なら選択中のパネルを動かす
+  if(this.isselected){
+    //propsとして渡している(x,y)をマウスに合わせて移動
+    //パネルとカーソルの間の差分を埋め合わせて常にパネルの同じ部分をつかめるようにする
+    this.panel_data[this.selectedindex].x = event.x - this.panel_data[this.selectedindex].x_offset
+    this.panel_data[this.selectedindex].y = event.y - this.panel_data[this.selectedindex].y_offset
+  }
+}
 </script>
 
 <template>
   <header>
-    <div class="frame">
-      <panel letter="あ" status="hit" />
-      <panel letter="い" status="blow"/>
-      <panel letter="う" status="miss"/>
-      <div class="square"></div>
-    </div>
   </header>
-
   <main>
+    <!--画面全体を覆うフレーム:マウスの位置を常に検知するために使用-->
+    <div class="frame" @mousemove="mousemove($event)">
+      <!--各パネル:配列上のデータをpropsとして渡す-->
+      <panel v-for="(pd, index) in panel_data" 
+        :letter="pd.letter" 
+        :status="pd.status"
+        :position="{x:pd.x, y:pd.y}"
+        :offset="{x_offset:pd.x_offset, y_offset:pd.y_offset}"
+        :isselect="pd.selected"
+        @mousedown="mousedown($event,index)"
+        @mouseup="mouseup($event,index)"
+      />
+    </div>
   </main>
 </template>
 
 <style scoped>
 .frame {
-  
+  position: absolute;
+  top: 0;
+  left: 0;
+  width:100%;
+  height:100%;
+  background-color: aquamarine;
 }
+
 </style>
--- src/components/panel.vue
+++ src/components/panel.vue
@@ -1,45 +1,37 @@
 <script setup>
-import { reactive, ref } from 'vue'
-
+import { onMounted, reactive, ref } from 'vue'
 defineProps({
   letter : {
     type: String,
     required: true
   },
   status : {
-
     type: String,
+    required: true
+  },
+  position : {
+    required: true
+  },
+  offset : {
+    required: true
+  },
+  isselect : {
+    type: Boolean,
     required: true
   }
 })
 
-const offset = reactive({
-  x: -50,
-  y: -50,
-})
 
-const isClicked = ref(false);
-
-function mousedown(event) {
-  this.isClicked = true;
-  this.offset.y = event.y - parseInt(event.target.style.top)
-  this.offset.x = event.x - parseInt(event.target.style.left)
-}
-
-function move(event){
-  if(!this.isClicked) return
-  event.target.style.top = (event.y - this.offset.y) + 'px'
-  event.target.style.left = (event.x - this.offset.x) +'px'
-}
 </script>
 
 <template>
-  <span style="top:0px; left:0px" class="panel" :class="status" @mousedown="mousedown($event)" @mousemove="move($event)" @mouseup="isClicked=false">
+  <span class="panel" :class="status, {'selecting': isselect}" :style="{top:position.y+'px', left:position.x+'px'}">
     <p class="letter">{{ letter }}</p>
   </span>
 </template>
 
 <style scoped>
+/*パネルの基礎デザイン*/
 .panel {
   /*パネル内の文字を中央ぞろえ*/
   display: flex;
@@ -60,35 +52,34 @@ function move(event){
   top:0px;
   left:0px;
 }
+/*選択中のパネル*/
+.selecting {
+  transform:scale(1.2,1.2);
+}
 
+/*パネルデザイン(不正解)*/
 .miss {
-  /*パネルデザイン(不正解)*/
   border-radius: 2vw;
   background-color: #eee;
   box-shadow: 0 0.5vw #ccc;
 }
-
+/*パネルデザイン(同じ行または列)*/
 .blow {
-  /*パネルデザイン(同じ行または列)*/
   border-radius: 2vw;
   background-color: #eb3;
   box-shadow: 0 0.5vw #c91;
   color: #fff;
 }
-
+/*パネルデザイン(正解)*/
 .hit {
-  /*パネルデザイン(正解)*/
   border-radius: 2vw;
   background-color: #6b5;
   box-shadow: 0 0.5vw #593;
   color: #fff;
 }
-
+/*文字を選択禁止&文字のマウスイベントを止める*/
 .letter {
-  /*文字を選択禁止にする*/
   user-select: none;
-  /*文字のクリックイベントを止める*/
   pointer-events: none;
-
 }
 </style>
