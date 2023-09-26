149b7493b2c567d51abbe65d89641d868e40e81d
tour_2st <fk.easel@gmail.com>
2023-09-26 22:38:56 +0900

    [added] 1-frame component added for get infomation about player's operation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
 import panel from './components/panel.vue'
+import one_frame from './components/1-frame.vue'
 import { reactive, ref } from 'vue'
 
-var selectedindex = ref(0)
-var isselected = ref(false)
+const selectedindex = ref(0)
+const isselected = ref(false)
+
+const start = reactive({
+  x: 0,
+  y: 0
+})
+
+const start_panel = -1;
+const end_panel = -1;
+
+const one_panels = reactive([{x:0, y:0, index: 0},{x:1, y:0, index: 1},{x:2, y:0, index: 2}, {x:3, y:0, index: 3},{x:4, y:0, index: 4},
+                             {x:0, y:1, index: 5},                     {x:2, y:1, index: 7},                      {x:4, y:1, index: 9},
+                             {x:0, y:2, index:10},{x:1, y:2, index:11},{x:2, y:2, index:12}, {x:3, y:2, index:13},{x:4, y:2, index:14},
+                             {x:0, y:3, index:15},                     {x:2, y:3, index:17},                      {x:4, y:3, index:19},
+                             {x:0, y:4, index:20},{x:1, y:4, index:21},{x:2, y:4, index:22}, {x:3, y:4, index:23},{x:4, y:4, index:24}])
 
 const panel_data = reactive([
   {
@@ -32,7 +47,169 @@ const panel_data = reactive([
     y_offset: 0,
     status: "miss",
     selected: false
-  }
+  },
+  {
+    letter: "あ",
+    x: 0,
+    y: 100,
+    x_offset: 0,
+    y_offset: 0,
+    status: "hit",
+    selected: false
+  },
+  {
+    letter: "い",
+    x: 0,
+    y: 0,
+    x_offset: 0,
+    y_offset: 0,
+    status: "blow",
+    selected: false
+  },
+  {
+    letter: "う",
+    x: 0,
+    y: 200,
+    x_offset: 0,
+    y_offset: 0,
+    status: "miss",
+    selected: false
+  },
+  {
+    letter: "あ",
+    x: 0,
+    y: 100,
+    x_offset: 0,
+    y_offset: 0,
+    status: "hit",
+    selected: false
+  },
+  {
+    letter: "い",
+    x: 0,
+    y: 0,
+    x_offset: 0,
+    y_offset: 0,
+    status: "blow",
+    selected: false
+  },
+  {
+    letter: "う",
+    x: 0,
+    y: 200,
+    x_offset: 0,
+    y_offset: 0,
+    status: "miss",
+    selected: false
+  },
+  {
+    letter: "あ",
+    x: 0,
+    y: 100,
+    x_offset: 0,
+    y_offset: 0,
+    status: "hit",
+    selected: false
+  },
+  {
+    letter: "い",
+    x: 0,
+    y: 0,
+    x_offset: 0,
+    y_offset: 0,
+    status: "blow",
+    selected: false
+  },
+  {
+    letter: "う",
+    x: 0,
+    y: 200,
+    x_offset: 0,
+    y_offset: 0,
+    status: "miss",
+    selected: false
+  },
+  {
+    letter: "あ",
+    x: 0,
+    y: 100,
+    x_offset: 0,
+    y_offset: 0,
+    status: "hit",
+    selected: false
+  },
+  {
+    letter: "い",
+    x: 0,
+    y: 0,
+    x_offset: 0,
+    y_offset: 0,
+    status: "blow",
+    selected: false
+  },
+  {
+    letter: "う",
+    x: 0,
+    y: 200,
+    x_offset: 0,
+    y_offset: 0,
+    status: "miss",
+    selected: false
+  },
+  {
+    letter: "あ",
+    x: 0,
+    y: 100,
+    x_offset: 0,
+    y_offset: 0,
+    status: "hit",
+    selected: false
+  },
+  {
+    letter: "い",
+    x: 0,
+    y: 0,
+    x_offset: 0,
+    y_offset: 0,
+    status: "blow",
+    selected: false
+  },
+  {
+    letter: "う",
+    x: 0,
+    y: 200,
+    x_offset: 0,
+    y_offset: 0,
+    status: "miss",
+    selected: false
+  },
+  {
+    letter: "あ",
+    x: 0,
+    y: 100,
+    x_offset: 0,
+    y_offset: 0,
+    status: "hit",
+    selected: false
+  },
+  {
+    letter: "い",
+    x: 0,
+    y: 0,
+    x_offset: 0,
+    y_offset: 0,
+    status: "blow",
+    selected: false
+  },
+  {
+    letter: "う",
+    x: 0,
+    y: 200,
+    x_offset: 0,
+    y_offset: 0,
+    status: "miss",
+    selected: false
+  },
 ])
 
 //パネル上でマウスを押したときの動作
@@ -47,18 +224,33 @@ function mousedown(event, index)
   this.panel_data[this.selectedindex].y_offset = event.y - this.panel_data[this.selectedindex].y
 
   this.panel_data[this.selectedindex].selected = true
+
+  this.start.x = this.panel_data[this.selectedindex].x
+  this.start.y = this.panel_data[this.selectedindex].y
+
 }
 //パネル上でマウスを離したときの動作
 function mouseup(event, index)
 {
+  console.log("mouseuped outher roam")
   //選択中を外す
   this.isselected = false
   this.panel_data[this.selectedindex].selected = false
   //選択中のインデックスと座標を用いて操作を実行
+
+  this.panel_data[this.selectedindex].x = event.x - this.panel_data[this.selectedindex].x_offset
+  this.panel_data[this.selectedindex].y = event.y - this.panel_data[this.selectedindex].y_offset
+
+  this.panel_data[this.selectedindex].x -= this.panel_data[this.selectedindex].x % (window.innerWidth*0.11).toFixed()
+  this.panel_data[this.selectedindex].y -= this.panel_data[this.selectedindex].y % (window.innerWidth*0.11).toFixed()
+  this.panel_data[this.selectedindex].x += (window.innerWidth*0.11).toFixed()/2
+  this.panel_data[this.selectedindex].y += (window.innerWidth*0.11).toFixed()/2
+
 }
 //画面上でマウスを操作した際の動作
 function mousemove(event)
 {
+  console.log(event.x/this.panel_size, event.y/this.panel_size)
   //選択中なら選択中のパネルを動かす
   if(this.isselected){
     //propsとして渡している(x,y)をマウスに合わせて移動
@@ -67,6 +259,24 @@ function mousemove(event)
     this.panel_data[this.selectedindex].y = event.y - this.panel_data[this.selectedindex].y_offset
   }
 }
+
+function clicked(event, index) {
+  console.log(index)
+  this.start_panel = index
+}
+
+function uped(event, index) {
+  console.log(index)
+  this.end_panel = index
+  console.log("change" + this.start_panel + " " + this.end_panel)
+}
+
+function otherclicked(event) {
+  console.log("other area is clicked")
+}
+
+const panel_size = ref((window.innerWidth*0.11).toFixed())
+
 </script>
 
 <template>
@@ -74,8 +284,14 @@ function mousemove(event)
   </header>
   <main>
     <!--画面全体を覆うフレーム:マウスの位置を常に検知するために使用-->
-    <div class="frame" @mousemove="mousemove($event)">
-      <!--各パネル:配列上のデータをpropsとして渡す-->
+    <div class="frame" @mousemove="mousemove($event)" @click="otherclicked($event)">
+      <one_frame v-for="(ops, index) in one_panels"
+        :position="{x:ops.x, y:ops.y}"
+        :size="panel_size"
+        @mousedown="clicked($event, ops.index)"
+        @mouseup="uped($event, ops.index)"
+      />
+      <!--各パネル:配列上のデータをpropsとして渡す
       <panel v-for="(pd, index) in panel_data" 
         :letter="pd.letter" 
         :status="pd.status"
@@ -84,7 +300,7 @@ function mousemove(event)
         :isselect="pd.selected"
         @mousedown="mousedown($event,index)"
         @mouseup="mouseup($event,index)"
-      />
+      />-->
     </div>
   </main>
 </template>
