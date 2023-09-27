d2bde53529f4ea168fea7f041097607294a95405
tour_2st <fk.easel@gmail.com>
2023-09-27 19:37:51 +0900

    [added] function to check answer and change status of panel is added

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,14 @@ const start = reactive({
   y: 0
 })
 
-const start_panel = -1;
-const end_panel = -1;
+const panel_size = ref((window.innerWidth*0.11).toFixed())
 
+const answer = [["あ","い","う","あ","あ"],
+                ["あ","_","え","_","あ"],
+                ["あ","あ","お","あ","あ"],
+                ["あ","_","あ","_","あ"],
+                ["あ","あ","あ","あ","あ"]]
+              
 const one_panels = reactive([{x:0, y:0, index: 0},{x:1, y:0, index: 1},{x:2, y:0, index: 2}, {x:3, y:0, index: 3},{x:4, y:0, index: 4},
                              {x:0, y:1, index: 5},                     {x:2, y:1, index: 7},                      {x:4, y:1, index: 9},
                              {x:0, y:2, index:10},{x:1, y:2, index:11},{x:2, y:2, index:12}, {x:3, y:2, index:13},{x:4, y:2, index:14},
@@ -24,191 +29,277 @@ const panel_data = reactive([
   {
     letter: "あ",
     x: 0,
-    y: 100,
+    y: 0,
     x_offset: 0,
     y_offset: 0,
     status: "hit",
-    selected: false
+    selected: false,
+    size: panel_size
+
   },
   {
     letter: "い",
-    x: 0,
+    x: 1,
     y: 0,
     x_offset: 0,
     y_offset: 0,
     status: "blow",
-    selected: false
+    selected: false,
+    size: panel_size
+
   },
   {
     letter: "う",
-    x: 0,
-    y: 200,
+    x: 2,
+    y: 0,
     x_offset: 0,
     y_offset: 0,
     status: "miss",
-    selected: false
+    selected: false,
+    size: panel_size
+
   },
   {
-    letter: "あ",
-    x: 0,
-    y: 100,
+    letter: "え",
+    x: 3,
+    y: 0,
     x_offset: 0,
     y_offset: 0,
     status: "hit",
-    selected: false
+    selected: false,
+    size: panel_size
+
   },
   {
-    letter: "い",
-    x: 0,
+    letter: "お",
+    x: 4,
     y: 0,
     x_offset: 0,
     y_offset: 0,
     status: "blow",
-    selected: false
+    selected: false,
+    size: panel_size
+
   },
   {
-    letter: "う",
+    letter: "か",
     x: 0,
-    y: 200,
+    y: 1,
     x_offset: 0,
     y_offset: 0,
     status: "miss",
-    selected: false
+    selected: false,
+    size: panel_size
+
   },
   {
-    letter: "あ",
-    x: 0,
-    y: 100,
+    letter: "き",
+    x: 1,
+    y: 1,
     x_offset: 0,
     y_offset: 0,
-    status: "hit",
-    selected: false
+    status: "nothing",
+    selected: false,
+    size: panel_size,
+
   },
   {
-    letter: "い",
-    x: 0,
-    y: 0,
+    letter: "く",
+    x: 2,
+    y: 1,
     x_offset: 0,
     y_offset: 0,
     status: "blow",
-    selected: false
+    selected: false,
+    size: panel_size
+
   },
   {
-    letter: "う",
-    x: 0,
-    y: 200,
+    letter: "け",
+    x: 1,
+    y: 1,
     x_offset: 0,
     y_offset: 0,
-    status: "miss",
-    selected: false
+    status: "nothing",
+    selected: false,
+    size: panel_size,
+
   },
   {
-    letter: "あ",
-    x: 0,
-    y: 100,
-    x_offset: 0,
-    y_offset: 0,
-    status: "hit",
-    selected: false
-  },
-  {
-    letter: "い",
-    x: 0,
-    y: 0,
-    x_offset: 0,
-    y_offset: 0,
-    status: "blow",
-    selected: false
-  },
-  {
-    letter: "う",
-    x: 0,
-    y: 200,
-    x_offset: 0,
-    y_offset: 0,
-    status: "miss",
-    selected: false
-  },
-  {
-    letter: "あ",
-    x: 0,
-    y: 100,
+    letter: "こ",
+    x: 4,
+    y: 1,
     x_offset: 0,
     y_offset: 0,
     status: "hit",
-    selected: false
+    selected: false,
+    size: panel_size
+
   },
   {
-    letter: "い",
+    letter: "さ",
     x: 0,
-    y: 0,
+    y: 2,
     x_offset: 0,
     y_offset: 0,
     status: "blow",
-    selected: false
+    selected: false,
+    size: panel_size
+
   },
   {
-    letter: "う",
-    x: 0,
-    y: 200,
+    letter: "し",
+    x: 1,
+    y: 2,
     x_offset: 0,
     y_offset: 0,
     status: "miss",
-    selected: false
+    selected: false,
+    size: panel_size
+
   },
   {
-    letter: "あ",
-    x: 0,
-    y: 100,
+    letter: "す",
+    x: 2,
+    y: 2,
     x_offset: 0,
     y_offset: 0,
     status: "hit",
-    selected: false
+    selected: false,
+    size: panel_size
+
   },
   {
-    letter: "い",
-    x: 0,
-    y: 0,
+    letter: "せ",
+    x: 3,
+    y: 2,
     x_offset: 0,
     y_offset: 0,
     status: "blow",
-    selected: false
+    selected: false,
+    size: panel_size
+
   },
   {
-    letter: "う",
-    x: 0,
-    y: 200,
+    letter: "そ",
+    x: 4,
+    y: 2,
     x_offset: 0,
     y_offset: 0,
     status: "miss",
-    selected: false
+    selected: false,
+    size: panel_size
+
   },
   {
-    letter: "あ",
+    letter: "た",
     x: 0,
-    y: 100,
+    y: 3,
     x_offset: 0,
     y_offset: 0,
     status: "hit",
-    selected: false
+    selected: false,
+    size: panel_size
+
   },
   {
-    letter: "い",
-    x: 0,
-    y: 0,
+    letter: "ち",
+    x: 1,
+    y: 1,
     x_offset: 0,
     y_offset: 0,
-    status: "blow",
-    selected: false
+    status: "nothing",
+    selected: false,
+    size: panel_size,
+
   },
   {
-    letter: "う",
-    x: 0,
-    y: 200,
+    letter: "つ",
+    x: 2,
+    y: 3,
     x_offset: 0,
     y_offset: 0,
     status: "miss",
-    selected: false
+    selected: false,
+    size: panel_size
+
+  },
+  {
+    letter: "て",
+    x: 1,
+    y: 1,
+    x_offset: 0,
+    y_offset: 0,
+    status: "nothing",
+    selected: false,
+    size: panel_size,
+
+  },
+  {
+    letter: "そ",
+    x: 4,
+    y: 3,
+    x_offset: 0,
+    y_offset: 0,
+    status: "blow",
+    selected: false,
+    size: panel_size
+
+  },
+  {
+    letter: "な",
+    x: 0,
+    y: 4,
+    x_offset: 0,
+    y_offset: 0,
+    status: "miss",
+    selected: false,
+    size: panel_size
+
+  },
+  {
+    letter: "に",
+    x: 1,
+    y: 4,
+    x_offset: 0,
+    y_offset: 0,
+    status: "hit",
+    selected: false,
+    size: panel_size
+
+  },
+  {
+    letter: "ぬ",
+    x: 2,
+    y: 4,
+    x_offset: 0,
+    y_offset: 0,
+    status: "blow",
+    selected: false,
+    size: panel_size
+
+  },
+  {
+    letter: "ね",
+    x: 3,
+    y: 4,
+    x_offset: 0,
+    y_offset: 0,
+    status: "miss",
+    selected: false,
+    size: panel_size
+
+  },
+  {
+    letter: "の",
+    x: 4,
+    y: 4,
+    x_offset: 0,
+    y_offset: 0,
+    status: "hit",
+    selected: false,
+    size: panel_size
+
   },
 ])
 
@@ -220,63 +311,120 @@ function mousedown(event, index)
   this.isselected = true
 
   //パネルとカーソルの間の差分
-  this.panel_data[this.selectedindex].x_offset = event.x - this.panel_data[this.selectedindex].x
-  this.panel_data[this.selectedindex].y_offset = event.y - this.panel_data[this.selectedindex].y
-
+  
+  this.panel_data[this.selectedindex].x_offset = (event.x/this.panel_size - this.panel_data[this.selectedindex].x)
+  this.panel_data[this.selectedindex].y_offset = (event.y/this.panel_size - this.panel_data[this.selectedindex].y)
+  
   this.panel_data[this.selectedindex].selected = true
 
   this.start.x = this.panel_data[this.selectedindex].x
   this.start.y = this.panel_data[this.selectedindex].y
 
 }
-//パネル上でマウスを離したときの動作
-function mouseup(event, index)
+
+//画面上でマウスを操作した際の動作
+function mousemove(event)
 {
-  console.log("mouseuped outher roam")
+  //選択中なら選択中のパネルを動かす
+  if(this.isselected){
+    //propsとして渡している(x,y)をマウスに合わせて移動
+    //パネルとカーソルの間の差分を埋め合わせて常にパネルの同じ部分をつかめるようにする
+    this.panel_data[this.selectedindex].x = event.x/this.panel_size - this.panel_data[this.selectedindex].x_offset
+    this.panel_data[this.selectedindex].y = event.y/this.panel_size - this.panel_data[this.selectedindex].y_offset
+  }
+}
+
+function clicked(event, index) {
+  if(this.isselected || this.panel_data[index].status=="hit") return
+  console.log(index)
+  this.start_panel = index
+  //selectedindex を 選択中にする
+  this.selectedindex = index
+  this.isselected = true
+
+  //パネルとカーソルの間の差分
+  
+  this.panel_data[this.selectedindex].x_offset = (event.x/this.panel_size - this.panel_data[this.selectedindex].x)
+  this.panel_data[this.selectedindex].y_offset = (event.y/this.panel_size - this.panel_data[this.selectedindex].y)
+  
+  this.panel_data[this.selectedindex].selected = true
+
+  this.start.x = this.panel_data[this.selectedindex].x
+  this.start.y = this.panel_data[this.selectedindex].y
+}
+
+function uped(event, index) {
+  if(this.isselected==false) return
+  if(this.panel_data[index].status == "hit")
+  {
+    console.log("!")
+    this.otherclicked(event)
+    return 
+  }
+  console.log(index)
+  this.end_panel = index
+  console.log("change" + this.start_panel + " " + this.end_panel)
   //選択中を外す
   this.isselected = false
   this.panel_data[this.selectedindex].selected = false
   //選択中のインデックスと座標を用いて操作を実行
 
-  this.panel_data[this.selectedindex].x = event.x - this.panel_data[this.selectedindex].x_offset
-  this.panel_data[this.selectedindex].y = event.y - this.panel_data[this.selectedindex].y_offset
+  this.panel_data[this.selectedindex].x = event.x/this.panel_size - this.panel_data[this.selectedindex].x_offset 
+  this.panel_data[this.selectedindex].y = event.y/this.panel_size - this.panel_data[this.selectedindex].y_offset 
 
-  this.panel_data[this.selectedindex].x -= this.panel_data[this.selectedindex].x % (window.innerWidth*0.11).toFixed()
-  this.panel_data[this.selectedindex].y -= this.panel_data[this.selectedindex].y % (window.innerWidth*0.11).toFixed()
-  this.panel_data[this.selectedindex].x += (window.innerWidth*0.11).toFixed()/2
-  this.panel_data[this.selectedindex].y += (window.innerWidth*0.11).toFixed()/2
+  this.panel_data[this.selectedindex].x = this.panel_data[this.selectedindex].x.toFixed()
+  this.panel_data[this.selectedindex].y = this.panel_data[this.selectedindex].y.toFixed()
 
-}
-//画面上でマウスを操作した際の動作
-function mousemove(event)
-{
-  console.log(event.x/this.panel_size, event.y/this.panel_size)
-  //選択中なら選択中のパネルを動かす
-  if(this.isselected){
-    //propsとして渡している(x,y)をマウスに合わせて移動
-    //パネルとカーソルの間の差分を埋め合わせて常にパネルの同じ部分をつかめるようにする
-    this.panel_data[this.selectedindex].x = event.x - this.panel_data[this.selectedindex].x_offset
-    this.panel_data[this.selectedindex].y = event.y - this.panel_data[this.selectedindex].y_offset
-  }
-}
+  console.log("changed!!")
+  this.panel_data[index].x = this.start.x
+  this.panel_data[index].y = this.start.y
 
-function clicked(event, index) {
-  console.log(index)
-  this.start_panel = index
-}
+  const temp = this.panel_data[index]
+  this.panel_data[index] = this.panel_data[this.selectedindex]
+  this.panel_data[this.selectedindex] = temp
 
-function uped(event, index) {
-  console.log(index)
-  this.end_panel = index
-  console.log("change" + this.start_panel + " " + this.end_panel)
+  this.check_answer();
 }
 
 function otherclicked(event) {
   console.log("other area is clicked")
+  console.log(this.start_panel)
+  //選択中を外す
+  this.isselected = false
+  this.panel_data[this.selectedindex].selected = false
+  //選択中のインデックスと座標を用いて操作を実行
+
+  this.panel_data[this.selectedindex].x = this.start.x
+  this.panel_data[this.selectedindex].y = this.start.y
+
 }
 
-const panel_size = ref((window.innerWidth*0.11).toFixed())
-
+function check_answer(){
+  for(let i=0; i<5; i++) for(let j=0; j<5; j++) if(this.answer[i][j]!="_"){
+    if(this.answer[i][j]==this.panel_data[5*i+j].letter){
+      console.log(i, j)
+      console.log(this.panel_data[5*i+j].status)
+      this.answer[i][j] = "_"
+      this.panel_data[5*i+j].status = "hit"
+    }
+  }
+  for(let i=0; i<5; i++) for(let j=0; j<5; j++) if(this.answer[i][j]!="_"){
+    console.log(i, j)
+    const is_blow = false;
+    for(let k=0; k<5; k++) if(!this.is_blow && this.answer[k][j]==this.panel_data[5*i+j].letter){
+      this.is_blow = true
+      break
+    }
+    for(let k=0; k<5; k++) if(!this.is_blow && this.answer[i][k]==this.panel_data[5*i+j].letter){
+      this.is_blow = true
+      break
+    }
+    if(this.is_blow) this.panel_data[5*i+j].status = "blow" 
+    else this.panel_data[5*i+j].status = "miss"
+    console.log(this.panel_data[5*i+j].status);
+    this.is_blow = false
+  }
+}
 </script>
 
 <template>
@@ -284,23 +432,21 @@ const panel_size = ref((window.innerWidth*0.11).toFixed())
   </header>
   <main>
     <!--画面全体を覆うフレーム:マウスの位置を常に検知するために使用-->
-    <div class="frame" @mousemove="mousemove($event)" @click="otherclicked($event)">
+    <div class="frame" @mousemove="mousemove($event)" @mouseup="otherclicked($event)">
       <one_frame v-for="(ops, index) in one_panels"
         :position="{x:ops.x, y:ops.y}"
         :size="panel_size"
         @mousedown="clicked($event, ops.index)"
-        @mouseup="uped($event, ops.index)"
+        @mouseup.stop="uped($event, ops.index)"
       />
-      <!--各パネル:配列上のデータをpropsとして渡す
+      <!--各パネル:配列上のデータをpropsとして渡す-->
       <panel v-for="(pd, index) in panel_data" 
         :letter="pd.letter" 
         :status="pd.status"
         :position="{x:pd.x, y:pd.y}"
         :offset="{x_offset:pd.x_offset, y_offset:pd.y_offset}"
         :isselect="pd.selected"
-        @mousedown="mousedown($event,index)"
-        @mouseup="mouseup($event,index)"
-      />-->
+      />
     </div>
   </main>
 </template>
--- src/components/panel.vue
+++ src/components/panel.vue
@@ -18,6 +18,9 @@ defineProps({
   isselect : {
     type: Boolean,
     required: true
+  },
+  size : {
+    required: false
   }
 })
 
@@ -25,7 +28,7 @@ defineProps({
 </script>
 
 <template>
-  <span class="panel" :class="status, {'selecting': isselect}" :style="{top:position.y+'px', left:position.x+'px'}">
+  <span class="panel" :class="status, {'selecting': isselect}" :style="{top:position.y*11+'vw', left:position.x*11+'vw'}">
     <p class="letter">{{ letter }}</p>
   </span>
 </template>
@@ -33,6 +36,7 @@ defineProps({
 <style scoped>
 /*パネルの基礎デザイン*/
 .panel {
+  transition-duration: 100ms;
   /*パネル内の文字を中央ぞろえ*/
   display: flex;
   align-items: center;
@@ -51,31 +55,41 @@ defineProps({
   position: absolute;
   top:0px;
   left:0px;
+
+  border-radius: 2vw;
+  border-style: solid;
+  border-width: 0.1vw;
+
+  pointer-events: none;
 }
 /*選択中のパネル*/
 .selecting {
+  transition-duration: 0ms;
   transform:scale(1.2,1.2);
 }
 
 /*パネルデザイン(不正解)*/
 .miss {
-  border-radius: 2vw;
   background-color: #eee;
   box-shadow: 0 0.5vw #ccc;
+  border-color: #eee;
 }
 /*パネルデザイン(同じ行または列)*/
 .blow {
-  border-radius: 2vw;
   background-color: #eb3;
   box-shadow: 0 0.5vw #c91;
+  border-color: #eb3;
   color: #fff;
 }
 /*パネルデザイン(正解)*/
 .hit {
-  border-radius: 2vw;
   background-color: #6b5;
   box-shadow: 0 0.5vw #593;
+  border-color: #6b5;
   color: #fff;
+}
+.nothing {
+  visibility: hidden;
 }
 /*文字を選択禁止&文字のマウスイベントを止める*/
 .letter {
